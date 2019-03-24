0af8a0b9f4e666288db679eb5cd6b88d579bc541
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2019-03-23 19:30:40 -0700

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = [str(ir.req) for ir in reqs]
 
 setuptools.setup(
     name="notion",
-    version="0.0.1",
+    version="0.0.2",
     author="Maxwell Huang-Hobbs",
     author_email="mhuan13+pypy@gmail.com",
     description="A tool to sync a notion collection to markdown files",
