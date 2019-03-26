1c02c7dc15311711a6190ca939b7128634e1a2be
Maxwell Huang-Hobbs <mhuan13@gmail.com>
2019-03-25 20:25:13 -0700

    add push tags to travis script

--- scripts/push-and-publish-if-changed.sh
+++ scripts/push-and-publish-if-changed.sh
@@ -4,5 +4,6 @@ git log -1
 git diff --exit-code master origin/master
 if [[ "$?" != "0" ]]; then
     git push https://$GITHUB_TOKEN:x-oauth-basic@$GITHUB_REMOTE master:master
+    git push https://$GITHUB_TOKEN:x-oauth-basic@$GITHUB_REMOTE --tags
 	make publish-travis
 fi
